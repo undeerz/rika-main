0fe54a16a3400c66b6ed0c41347dccb6b2854c61
undeerz <pedrofilhodev@gmail.com>
2022-04-28 19:47:20 -0300

    update schemas

--- src/Schemas/userData.js
+++ src/Schemas/userData.js
@@ -2,6 +2,7 @@ const { Schema, model } = require('mongoose');
 
 module.exports = model('User',
 	new Schema({
-		id: { type: String, required: true }
+		id : { type: String, required: true },
+		premium : {type: Boolean, default: false},
 	})
 );
