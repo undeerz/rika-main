4d3e225233f3db8617b77aeff592e577fdba5cb9
undeerz <pedrofilhodev@gmail.com>
2022-05-03 17:40:35 -0300

    new commands

--- src/Commands/Message/Mod/Ban.js
+++ src/Commands/Message/Mod/Ban.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require("discord.js");
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const Command = require("../../../Structures/Command");
 
 module.exports = class extends Command {
@@ -11,17 +11,85 @@ module.exports = class extends Command {
     });
   }
 
-  async run(message) {
+  async run(message, args) {
+    const member = await this.client.getUser(args[0], message);
+    if (!args[0]) {
+      return message.reply("Mencione o membro");
+    }
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "Não informado";
+
     const userData = await this.client.userData.findOne({
       id: message.author.id,
     });
 
     const gif = userData.premium != false ? `${userData.gifban}` : "";
 
-    const embed = new MessageEmbed()
-    .setDescription("Test")
-    .setImage(`${gif}`);
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
+      return message.reply("Você não tem permissão");
+    }
 
-    return message.reply({ embeds: [embed] });
+    if (member == message.author.id) {
+      return message.reply("Você não pode banir a si mesmo");
+    }
+
+    const row = new MessageActionRow();
+    const confirm = new MessageButton()
+      .setCustomId("confirm")
+      .setLabel("Confirmar")
+      .setStyle("SUCCESS");
+
+    const cancel = new MessageButton()
+      .setCustomId("cancel")
+      .setLabel("Cancelar")
+      .setStyle("DANGER");
+
+    row.addComponents([confirm, cancel]);
+
+    const embed = new MessageEmbed()
+      .setAuthor({
+        name: message.author.username,
+        iconURL: message.author.avatarURL({ dynamic: true }),
+      })
+      .setDescription(`**Banir :** ${member} ?\n**Motivo :** ${reason}`);
+
+    const msg = await message.reply({ embeds: [embed], components: [row] });
+    let collect;
+
+    const filter = (interaction) => {
+      return interaction.isButton() && interaction.message.id === msg.id;
+    };
+
+    const collector = msg.createMessageComponentCollector({
+      filter: filter,
+      time: 60000,
+    });
+
+    collector.on("collect", async (x) => {
+      if (x.user.id != message.author.id) return x.deferUpdate();
+      collect = x;
+
+      switch (x.customId) {
+        case "confirm": {
+          msg.delete();
+
+          message.guild.members.ban(member);
+          const embed = new MessageEmbed()
+            .setDescription(
+              `${member} foi banido ${message.author}\nMotivo : ${reason}`
+            )
+            .setImage(`${gif}`);
+
+          message.reply({ embeds: [embed] });
+
+          break;
+        }
+      }
+    });
+
+    collector.on("end", (x) => {
+      if (collect) return;
+      msg.delete;
+    });
   }
 };
