4be84e990c569662c4539c81dc844d13ec31478a
undeerz <pedrofilhodev@gmail.com>
2022-05-11 18:47:09 -0300

    aa

--- src/Commands/Message/Fun/Slap.js
+++ src/Commands/Message/Fun/Slap.js
@@ -17,12 +17,10 @@ module.exports = class extends Command {
   async run(message, args) {
     let member = await this.client.getUser(args[0], message);
     if (!member)
-      return message.channel.send(
-        "User not found, try to mention it next time"
-      );
+      return message.channel.send(`» ${message.author}, user não encontrado.`);
     let img = await neko.fetchRandom("slap");
     const embed = new MessageEmbed()
-      .setDescription(`<@${message.author.id}> has slapped <@${member.id}>`)
+      .setDescription(`<@${message.author.id}> bateu no(a) <@${member.id}>`)
       .setImage(img.results[0].url);
 
     message.reply({ embeds: [embed] });
